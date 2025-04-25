e0b4b0b03500b755f5b92da1170378bf1206a98d
datand81 <dativo.andres@gmail.com>
2025-04-25 09:26:14 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'datand81.github.io-portfolio',
+    site: 'datand81.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
